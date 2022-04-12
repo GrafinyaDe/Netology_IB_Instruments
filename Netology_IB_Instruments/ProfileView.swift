46abc0450327480ded412425c347312269295ada
Sabina Tarasova <sibipka@gmail.com>
2022-04-12 14:06:11 +0400

    Привязка элементов к классу

---
+++ Netology_IB_Instruments/ProfileView.swift
@@ -0,0 +1,17 @@
+//
+//  ProfileView.swift
+//  Netology_IB_Instruments
+//
+//  Created by apple on 12.04.2022.
+//
+
+import UIKit
+
+class ProfileView: UIView {
+
+    @IBOutlet weak var fotoImageView: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var ageLabel: UILabel!
+    @IBOutlet weak var sityLabel: UILabel!
+    @IBOutlet weak var scrollTextView: UITextView!
+}
